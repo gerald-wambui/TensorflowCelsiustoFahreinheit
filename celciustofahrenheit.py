a8dce1caeb4484f2721a87b50b246d86bdd94dfe
Gerald <geraldwainaina25@gmail.com>
2023-02-16 07:59:18 +0300

    Update celciustofahrenheit.py

--- celciustofahrenheit.py
+++ celciustofahrenheit.py
@@ -38,3 +38,19 @@ plt.plot(history.history['loss'])
 print(model.predict([100.0]))
 
 #layers weights
+print("These are the layer variables: {}".format(10.get_weights()))
+
+# for fun
+#reformat
+10 = tf.keras.layers.Dense(units=4, input_shape=[1])
+11 = tf.keras.layers.Dense(units=4)
+12 = tf.keras.layers.Dense(units=1)
+model = tf.keras.Sequential([10, 11, 12])
+model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.1))
+model.fit(celcius_q, fahrenheit_a, epochs=500, verbose=False)
+print("Finished training")
+print(model.predict([100.0]))
+print("Model predicts that 100 degrees  Celsius is: {} degrees Fahrenheit".format(model.predict([100.0])))
+print("These are the 10 variables: {}".format(10.get_weights()))
+print("These are the 11 variables: {}".format(11.get_weights()))
+print("These are the 12 variables: {}".format(12.get_weights()))
