1748ca8652d7d24c05e4dab27069b345b84a0683
Gerald <geraldwainaina25@gmail.com>
2023-02-16 07:32:46 +0300

    Create celciustofahrenheit.py

---
+++ celciustofahrenheit.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import numpy as np
+import logging
+logger = tf.get_logger()
+logger.setLevel(logging.ERROR)
+
+#training data
+
+celcius_q = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
+fahrenheit_a = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
+
+for i,c in enumerate(celcius_q):
+    print("{} degrees Celsius = {} degrees Fahrenheit".format(c, fahrenheit_a[i]))
+
+#creat the model
